c9709a886bcccc3812fec61cb7a40cf2db3ab742
AbdullahAymanII <abdayman1092002@gmail.com>
2024-06-19 12:13:36 +0300

    feature-1 commit

--- src/FeatureOne.java
+++ src/FeatureOne.java
@@ -3,5 +3,6 @@ package src;
 public class FeatureOne {
     public static void main(String[] args) {
         System.out.println("add feature-1");
+        System.out.println("feature-1 ready to finish");
     }
 }
